3842db20c4b3bc109bf0f397b8d017401942ce54
Pawal Karki <pawal_karki@icloud.com>
2025-12-21 20:15:00 +0545

    feat(providers): add editor and modal providers

---
+++ src/providers/modal-provider.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import React from "react";
+import {
+  type Plan,
+  type Agency,
+  type Contact,
+  type User,
+} from "@prisma/client";
+import type { PriceList, TicketDetails } from "@/lib/types";
+
+export interface ModalData {
+  user?: User;
+  agency?: Agency;
+  contact?: Contact;
+  ticket?: TicketDetails;
+  plans?: {
+    defaultPriceId: Plan;
+    plans: PriceList["data"];
+  };
+}
+
+interface ModalContextType {
+  data: ModalData;
+  isOpen: boolean;
+  setOpen: (modal: React.ReactNode, fetch?: () => Promise<any>) => void;
+  setClose: () => void;
+}
+
+export const ModalContext = React.createContext<ModalContextType>({
+  data: {},
+  isOpen: false,
+  setOpen: (modal: React.ReactNode, fetch?: () => Promise<any>) => { },
+  setClose: () => { },
+});
+
+export const ModalProvider: React.FC<React.PropsWithChildren> = ({
+  children,
+}) => {
+  const [isOpen, setIsOpen] = React.useState<boolean>(false);
+  const [isMounted, setIsMounted] = React.useState<boolean>(false);
+  const [data, setData] = React.useState<ModalData>({});
+  const [currentModal, setCurrentModal] = React.useState<React.ReactNode>(null);
+
+  React.useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const setOpen: ModalContextType["setOpen"] = async (modal, fetch) => {
+    if (modal) {
+      if (fetch) {
+        const newData = await fetch();
+        setData({ ...data, ...newData } || {});
+      }
+
+      setCurrentModal(modal);
+      setIsOpen(true);
+    }
+  };
+
+  const setClose = () => {
+    setIsOpen(false);
+    setData({});
+  };
+
+  // Always render the Provider to maintain consistent hook count
+  // Only render modal content after client-side mount
+  return (
+    <ModalContext.Provider value={{ data, setOpen, setClose, isOpen }}>
+      {children}
+      {isMounted && currentModal}
+    </ModalContext.Provider>
+  );
+};
+
+export const useModal = () => {
+  const context = React.useContext(ModalContext);
+  if (!context) {
+    throw new Error("useModal must be used within the ModalProvider");
+  }
+  return context;
+};
+
+

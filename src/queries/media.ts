4111e48c5175f3fcf8cb22eade4a8a34e96b41b7
Pawal Karki <pawal_karki@icloud.com>
2025-12-23 09:20:00 +0545

    feat(queries): add query functions module

---
+++ src/queries/media.ts
@@ -0,0 +1,6 @@
+/**
+ * Media query functions
+ * Re-exports from the main queries file for better organization
+ */
+
+export { createMedia, deleteMedia, getMedia } from "@/lib/queries";
---
+++ src/queries/notifications.ts
@@ -0,0 +1,6 @@
+/**
+ * Notification query functions
+ * Re-exports from the main queries file for better organization
+ */
+
+export { saveActivityLogsNotification, getNotifications } from "@/lib/queries";

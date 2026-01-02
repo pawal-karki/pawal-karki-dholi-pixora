5ec9df7b1954b9ca7a1fcdcd3f5aa04496239ad2
Pawal Karki <pawal_karki@icloud.com>
2026-01-02 09:28:00 +0545

    feat(layout): update main layout structure

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import { ModalProvider } from "@/providers/modal-provider";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ClerkProvider appearance={{ baseTheme: dark }}>{children}</ClerkProvider>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
+      <ModalProvider>{children}</ModalProvider>
+    </ClerkProvider>
   );
 };
 

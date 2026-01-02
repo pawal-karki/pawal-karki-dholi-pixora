4b9670429e8a056e9af8f6c4ac0ad208b56ee462
Pawal Karki <pawal_karki@icloud.com>
2026-01-02 19:33:00 +0545

    feat(agency): add agency loading state

---
+++ src/app/(main)/agency/loading.tsx
@@ -0,0 +1,10 @@
+import Loading from "@/components/global/loading";
+
+export default function AgencyLoading() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <Loading />
+    </div>
+  );
+}
+

4b5e755f33a95293acd3605c16f9d57a78dbd0a2
Pawal Karki <pawal_karki@icloud.com>
2026-01-03 16:52:00 +0545

    feat(agency): add team management page

---
+++ src/app/(main)/agency/[agencyId]/team/columns.tsx
@@ -0,0 +1,270 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { type ColumnDef } from "@tanstack/react-table";
+import { Role } from "@prisma/client";
+import { toast } from "sonner";
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+
+import { deleteUser, getAuthUser, deleteInvitation } from "@/lib/queries";
+
+import { useModal } from "@/hooks/use-modal";
+import { Badge } from "@/components/ui/badge";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import UserDetails from "@/components/forms/user-details";
+import CustomModal from "@/components/global/custom-modal";
+
+import { cn } from "@/lib/utils";
+import { type UsersWithAgencySubAccountPermissionsSidebarOptions } from "@/lib/types";
+
+export const teamTableColumns: ColumnDef<UsersWithAgencySubAccountPermissionsSidebarOptions>[] =
+  [
+    {
+      accessorKey: "id",
+      header: "",
+      cell: () => {
+        return null;
+      },
+    },
+    {
+      accessorKey: "name",
+      header: "Name",
+      cell: ({ row }) => {
+        const avatarUrl = row.getValue("avatarUrl") as string;
+        const email = row.getValue("email") as string;
+
+        // Use DiceBear if avatarUrl is missing
+        const avatarSrc = avatarUrl || `https://api.dicebear.com/9.x/avataaars/svg?seed=${email}`;
+
+        return (
+          <div className="flex items-center gap-4">
+            <div className="h-11 w-11 relative flex-none">
+              <Image
+                src={avatarSrc}
+                fill
+                className="rounded-full object-cover"
+                alt="avatar image"
+              />
+            </div>
+            <span>{row.getValue("name")}</span>
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "avatarUrl",
+      header: "",
+      cell: () => {
+        return null;
+      },
+    },
+    { accessorKey: "email", header: "Email" },
+    {
+      accessorKey: "subAccounts",
+      header: "Owned Accounts",
+      cell: ({ row }) => {
+        const isAgencyOwner = row.getValue("role") === Role.AGENCY_OWNER;
+        const permissions = row.original?.permissions || [];
+        const ownedAccounts = Array.isArray(permissions)
+          ? permissions.filter((per) => per?.access)
+          : [];
+
+        if (isAgencyOwner)
+          return (
+            <div className="flex flex-col items-start">
+              <div className="flex flex-col gap-2">
+                <Badge className="bg-slate-600 whitespace-nowrap">
+                  Agency - {row?.original?.agency?.name}
+                </Badge>
+              </div>
+            </div>
+          );
+
+        return (
+          <div className="flex flex-col items-start">
+            <div className="flex flex-col gap-2">
+              {ownedAccounts?.length ? (
+                ownedAccounts.map((account) => (
+                  <Badge
+                    key={account.id}
+                    className="bg-slate-600 w-fit whitespace-nowrap"
+                  >
+                    Sub Account - {account.subAccount?.name}
+                  </Badge>
+                ))
+              ) : (
+                <div className="text-muted-foreground">No Access Yet</div>
+              )}
+            </div>
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "role",
+      header: "Role",
+      cell: ({ row }) => {
+        const role: Role = row.getValue("role");
+        return (
+          <Badge
+            className={cn({
+              "bg-emerald-500": role === Role.AGENCY_OWNER,
+              "bg-orange-400": role === Role.AGENCY_ADMIN,
+              "bg-primary": role === Role.SUBACCOUNT_USER,
+              "bg-muted": role === Role.SUBACCOUNT_GUEST,
+            })}
+          >
+            {role}
+          </Badge>
+        );
+      },
+    },
+    {
+      accessorKey: "status",
+      header: "Status",
+      cell: ({ row }) => {
+        const status = row.getValue("status") as string;
+        return (
+          <Badge
+            className={cn("bg-emerald-600", {
+              "bg-slate-500": status === "PENDING",
+            })}
+          >
+            {status || "ACTIVE"}
+          </Badge>
+        );
+      },
+    },
+    {
+      id: "actions",
+      cell: ({ row }) => {
+        const rowData = row.original;
+
+        return <CellActions rowData={rowData} />;
+      },
+    },
+  ];
+
+interface CellActionsProps {
+  rowData: UsersWithAgencySubAccountPermissionsSidebarOptions;
+}
+
+const CellActions: React.FC<CellActionsProps> = ({ rowData }) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const { setOpen } = useModal();
+
+  if (!rowData) return null;
+  if (!rowData.agency) return null;
+
+  return (
+    <AlertDialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuItem
+            className="flex gap-2"
+            onClick={() => navigator.clipboard.writeText(rowData?.email)}
+          >
+            <Copy size={15} /> Copy Email
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            className="flex gap-2"
+            onClick={() => {
+              setOpen(
+                <CustomModal
+                  title="Edit User Details"
+                  subTitle="You can change permissions only when the user has an owned subaccount"
+                >
+                  <UserDetails
+                    type="agency"
+                    id={rowData?.agency?.id || null}
+                    subAccounts={rowData?.agency?.SubAccounts}
+                  />
+                </CustomModal>,
+                async () => {
+                  return { user: await getAuthUser(rowData?.email) };
+                }
+              );
+            }}
+          >
+            <Edit className="w-4 h-4" />
+            Edit Details
+          </DropdownMenuItem>
+          {rowData.role !== "AGENCY_OWNER" && (
+            <AlertDialogTrigger asChild>
+              <DropdownMenuItem className="flex gap-2">
+                <Trash className="w-4 h-4" /> Remove User
+              </DropdownMenuItem>
+            </AlertDialogTrigger>
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle className="text-left">
+            Are you absolutely sure?
+          </AlertDialogTitle>
+          <AlertDialogDescription className="text-left">
+            This action cannot be undone. This will permanently delete the
+            {rowData.status === "PENDING" ? "invitation" : "user"} and related
+            data.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter className="flex items-center">
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            disabled={isLoading}
+            className="bg-destructive hover:bg-destructive"
+            onClick={async () => {
+              setIsLoading(true);
+              if (rowData.status === "PENDING") {
+                await deleteInvitation(rowData.id);
+                toast.success("Invitation Cancelled", {
+                  description: "The invitation has been cancelled.",
+                });
+              } else {
+                await deleteUser(rowData.id);
+                toast.success("Deleted User", {
+                  description: "The user has been deleted from this agency.",
+                });
+              }
+              setIsLoading(false);
+              router.refresh();
+            }}
+          >
+            Delete
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
---
+++ src/app/(main)/agency/[agencyId]/team/page.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { redirect } from "next/navigation";
+import { Plus } from "lucide-react";
+
+import { getAgencyDetails, getAuthUserGroup, getAuthDetails, getInvitationsByAgencyId } from "@/lib/queries";
+
+import TeamsDataTable from "./data-table";
+import { teamTableColumns } from "./columns";
+import SendInvitation from "@/components/forms/send-invitation";
+
+interface TeamPageProps {
+  params: Promise<{
+    agencyId: string | undefined;
+  }>;
+}
+
+const TeamPage: React.FC<TeamPageProps> = async ({ params }) => {
+  const { agencyId } = await params;
+  const user = await getAuthDetails();
+
+  if (!user) redirect("/agency/sign-in");
+  if (!agencyId) redirect("/agency/unauthorized");
+
+  const teamMembers = await getAuthUserGroup(agencyId);
+  if (!teamMembers) redirect("/agency/sign-in");
+
+  const agencyDetails = await getAgencyDetails(agencyId);
+  if (!agencyDetails) redirect("/agency/unauthorized");
+
+  const invitations = await getInvitationsByAgencyId(agencyId);
+
+  const mergedData = [
+    ...teamMembers.map((user) => ({
+      ...user,
+      permissions: user.Permissions || [],
+      status: "ACTIVE",
+    })),
+    ...invitations.map((invitation) => ({
+      id: invitation.id,
+      name: "Pending Invitation",
+      email: invitation.email,
+      role: invitation.role,
+      agency: agencyDetails,
+      avatarUrl: "",
+      agencyId: agencyId,
+      permissions: [],
+      status: "PENDING",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      password: null,
+    })),
+  ] as any;
+
+  return (
+    <TeamsDataTable
+      actionButtonText={
+        <>
+          <Plus className="w-4 h-4" />
+          Add
+        </>
+      }
+      modalChildren={<SendInvitation agencyId={agencyId} />}
+      filterValue="name"
+      columns={teamTableColumns}
+      data={mergedData}
+    />
+  );
+};
+
+export default TeamPage;
+
